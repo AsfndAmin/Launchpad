9e7c1649934316662e758a51996ea2921fe29d6c
Asfand Amin <99183246+AsfndAmin@users.noreply.github.com>
2023-03-06 13:04:38 +0500

    Update FairLaunchFactory.sol

--- LaunchPadContracts/FairLaunchContracts/FairLaunchFactory.sol
+++ LaunchPadContracts/FairLaunchContracts/FairLaunchFactory.sol
@@ -4,32 +4,108 @@ import "./DecentrapadFairLaunch.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract DecentraFairLaunchFactory {
+contract DecentraFairLaunchFactory is AccessControl{
     using SafeERC20 for ERC20;
     using SafeMath for uint256;
  
 
-    event launchpadAddress(address _poolAddress);
-    // need to add check when user enters the data about rates and tokens for liquidity and pool.
-    // revert if tokens for LP * rate > (tokens for sale * preSale rate) + platform fee
+    event poolDeployed(address _poolAddress, address _owner);
+
+    bytes32 public constant EXECUTOR_ROLE = keccak256("EXECUTOR_ROLE");
+
+    address platformAddress;
+    uint256 platformOneTimeFee;
+    uint8 nativeFee;
+    uint8 dualFees; 
+
+
+        constructor() {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(EXECUTOR_ROLE, msg.sender);
+    }
+
     function deployPool(
         DecentrapadFairLaunch.launchSaleInfo memory _launchInfo,
         DecentrapadFairLaunch.dexListingInfo memory _listingInfo,
         address owner,
-        bool _listingDex,
-        bool _referRewardStatus 
-    ) external {
-        uint256 totalTokensRequired = _launchInfo.tokensForSale.add(_listingInfo.maxTokensForLiquidity);
+        bool feesInNative, 
+        bool _referRewardStatus,
+        bool _maxContribution 
+    ) external payable{
+                require(
+            msg.value == platformOneTimeFee,
+            "Invalid Pool Creation Fee sent"
+        );
+
+        uint256 totalTokensRequired;
+        DecentrapadFairLaunch.poolFee memory _poolFee;
+
+        if (feesInNative) {
+            _poolFee = DecentrapadFairLaunch.poolFee(nativeFee, 0);
+            totalTokensRequired = calculateTotalForNative(_launchInfo.tokensForSale, nativeFee, _listingInfo.lpTokensRatio);
+        } else {
+            _poolFee = DecentrapadFairLaunch.poolFee(dualFees, dualFees);
+            totalTokensRequired = calculateTotalForDual(_launchInfo.tokensForSale, dualFees, _listingInfo.lpTokensRatio);
+        } 
+
         DecentrapadFairLaunch decentraPool = new DecentrapadFairLaunch(
             _launchInfo,
             _listingInfo,
+            _poolFee, 
             owner,
-            _listingDex,
-            _referRewardStatus
+            true,
+            _referRewardStatus,
+            _maxContribution,
+            platformAddress
+            
             
         );
         ERC20(_launchInfo.tokenAddress).safeTransferFrom(msg.sender, address(decentraPool), totalTokensRequired);
-        emit launchpadAddress(address(decentraPool));
+        payable(platformAddress).transfer(msg.value);
+        emit poolDeployed(address(decentraPool), owner);
     }
+    //when fee is native
+    function calculateTotalForNative(uint256 _tokensForSale, uint256 _fee, uint256 _lpTokenRatio) public pure returns(uint256){
+        uint256 tokensForLP = _tokensForSale.mul(_lpTokenRatio).div(1000);
+        uint256 calculate =  tokensForLP.mul(_fee).div(1000);
+        uint256 tokensAfterFee = tokensForLP.sub(calculate);
+        uint256 totalTokensRequired = _tokensForSale.add(tokensAfterFee);
+        return totalTokensRequired;
+    }
+
+    function calculateTotalForDual(uint256 _tokensForSale, uint256 _dualFee, uint256 _lpTokenRatio) public pure returns(uint256){
+        uint256 tokensForLP = _tokensForSale.mul(_lpTokenRatio).div(1000);
+        uint256 feeTokensToAdd = _tokensForSale.mul(_dualFee).div(1000);
+        uint256 feeTokensToSub = tokensForLP.mul(_dualFee).div(1000); 
+        uint256 totalTokensRequired = ((_tokensForSale.add(tokensForLP)).add(feeTokensToAdd)).sub(feeTokensToSub);
+        return totalTokensRequired;
+    }
+
+    function setPoolFees(uint8 _native, uint8 _dualFees)
+        external
+        onlyRole(EXECUTOR_ROLE)
+    {
+        nativeFee = _native;
+        dualFees = _dualFees;
+    }
+
+    function setPlaformAddress(address newAddress)
+        external
+        onlyRole(EXECUTOR_ROLE)
+    {
+        platformAddress = newAddress;
+    }
+
+    function setPlaformFee(uint256 newFee) external onlyRole(EXECUTOR_ROLE) {
+        platformOneTimeFee = newFee;
+    }
+
+    function getPlatformAddress() external view returns (address) {
+        return platformAddress;
+    }
+
+
+
 }
